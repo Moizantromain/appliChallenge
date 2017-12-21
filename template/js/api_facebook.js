f9d3c3462358c940e9bd796bad5df3e9a6fec929
Moizantromain <moizant.romain@gmail.com>
2017-12-21 15:57:10 +0100

    fuction API

--- template/js/api_facebook.js
+++ template/js/api_facebook.js
@@ -1,30 +1,37 @@
 function recherche(quoi) {
+    console.log(quoi);
     var url = "https://graph.facebook.com/search?";
-    var quoi = "le monde";
+    var quoi = "";
+    quoi = document.getElementById('quoi').value;
     var data_Query = "&q=" + quoi + "&type=page";
     var access_token = "&access_token=437660773298889|371758459896094a60e6c3b878aa947a";
     var url_data = url + data_Query + access_token;
 
     $.getJSON(url_data, function (result) {
-
         traiteDonnee(result);
-        // $.each(result, function (i, field) {
-        //     console.log(field);
-        //     $("div").append(field.name + " ");
-        // });
     });
 }
 
 function traiteDonnee(a) {
     var table = $('<table>').addClass("maClasse");
+    var Multimg = $('#Imgli').addClass("Multimg");
+    var multimg = $('#imgli').addClass("multimg");
     for (var i = 0; i < a.data.length; i++) {
         console.log(a);
-        var rangee = $('<tr>').addClass("maClasseRangee").text(a.data[i].id);
+        var li = ($('<li>').addClass("Multimg")).append($('<img>').addClass("multimg").attr('src', 'https://graph.facebook.com/' + a.data[i].id + '/picture/?width=80'));
+        var image = $('#Logo').attr('src', 'https://graph.facebook.com/' + a.data[0].id + '/picture/?width=80');
+        console.log(image);
         var rangee2 = $('<tr>').addClass("maClasseRangee").text(a.data[i].name);
-        table.append(rangee);
+        Multimg.append(li);
         table.append(rangee2);
     }
     $("#tableIci").append(table);
 }
 
-recherche();
+
+
+$("#AA1").click(function () {
+    $('#More').css("display", "block");
+
+
+});
